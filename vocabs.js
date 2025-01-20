1410d76a38aa8dd9c32c445dbb18aff272d1ff86
Daniel Brecher <danav.bred@gmail.com>
2025-01-20 06:23:02 +0200

    Initial commit of Simplos2025

---
+++ vocabs.js
@@ -0,0 +1,9 @@
+window.vocabularySets = {
+
+  "1_1": {
+stage: 1,
+set: 1,
+  words: ["stability", "get", "beside", "sad", "important", "pick", "fair", "computer", "place", "together", "start", "go", "brother", "teach", "women", "of", "man", "quiet", "sentence", "enter", "news", "few", "ago", "student", "nice", "or", "course", "many", "month", "idea", "alone", "if", "happy", "smart", "until", "number", "careful", "true", "practice", "grade", "under", "exercise", "bag", "fact", "clean", "dad", "now", "classroom", "cold", "hang"],
+  translations: ["יציבות", "לקבל", "בצד", "עצוב", "חשוב", "לבחור", "הוגן", "מחשב", "מקום", "יחד", "התחלה", "ללכת", "אח", "ללמד", "נשים", "של", "איש", "שקט", "משפט", "להיכנס", "חדשות", "מעט", "לפני", "סטודנט", "נחמד", "או", "קורס", "הרבה", "חודש", "רעיון", "לבד", "אם", "שמח", "חכם", "עד", "מספר", "זהיר", "אמיתי", "לתרגל", "ציון", "מתחת", "תרגיל", "תיק", "עובדה", "לנקות", "אבא", "עכשיו", "כיתה", "קר", "לתלות"]
+  },
+  };
